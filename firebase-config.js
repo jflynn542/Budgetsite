3365e215f02a5d916b3a4ab50e3fe4bf28911911
jflynn542 <C24439822@mytudublin.ie>
2026-05-18 15:48:05 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Paste your own Firebase web app config here from Firebase Console > Project settings > Your apps > Web app.
+// The site will not connect to Firebase until these placeholder values are replaced.
+export const firebaseConfig = {
+  apiKey: "PASTE_YOUR_API_KEY_HERE",
+  authDomain: "PASTE_YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "PASTE_YOUR_PROJECT_ID",
+  storageBucket: "PASTE_YOUR_PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID",
+  appId: "PASTE_YOUR_APP_ID"
+};
